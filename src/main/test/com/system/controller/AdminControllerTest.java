0bebecc1c247b5b04d79911b36befd5d4706d28f
vymv <925882085@qq.com>
2021-05-25 12:11:43 +0800

    添加测试login校验

--- src/main/test/com/system/controller/AdminControllerTest.java
+++ src/main/test/com/system/controller/AdminControllerTest.java
@@ -1,16 +1,27 @@
 package com.system.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
+import org.apache.shiro.web.subject.WebSubject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
 
 import static org.junit.Assert.*;
 
@@ -24,9 +35,45 @@ public class AdminControllerTest {
 
     private MockMvc mockMvc;
 
+
+
+
+
+    @Resource
+    org.apache.shiro.mgt.SecurityManager securityManager;
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private Subject subject;
+
+    private MockHttpServletRequest mockHttpServletRequest;
+    private MockHttpServletResponse mockHttpServletResponse;
+
+
+    //用户登录
+    private void login(String username, String password) {
+        subject = new WebSubject.Builder(mockHttpServletRequest, mockHttpServletResponse)
+                .buildWebSubject();
+        //用户密码
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password, true);
+        subject.login(token);
+        ThreadContext.bind(subject);
+    }
+
     @Before
     public void init() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        //mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        mockHttpServletRequest = new MockHttpServletRequest(webApplicationContext.getServletContext());
+        mockHttpServletResponse = new MockHttpServletResponse();
+        MockHttpSession mockHttpSession = new MockHttpSession(webApplicationContext.getServletContext());
+        mockHttpServletRequest.setSession(mockHttpSession);
+        SecurityUtils.setSecurityManager( securityManager);
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+        login("admin", "123");
+
     }
 
     @Test
